c027c8a956585746d24103279cc3b363e6ad3876
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2020-09-02 10:15:38 -0500

    Add flag to switch between local and grid crab jobs

--- test/crab_config/crab_script.py
+++ test/crab_config/crab_script.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+# for local testing
+doLocal = False
 # Take input parameters
 isData = False
 if sys.argv[2].split('=')[1] == "data":
@@ -12,28 +14,34 @@ year = sys.argv[3].split('=')[1]
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import * 
 
 #this takes care of converting the input files from CRAB
-from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
+if not doLocal :
+    from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
 
 from StandardModel.ZEMuAnalysis.runSkimModule import *
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
 
-#inputfile = ['/afs/cern.ch/user/p/pellicci/cernbox/ROOT/ZEMuAnalysis/NANOAOD/191120_10218V1/ZEMuAnalysis_pythia8_NANOAOD_2018_1.root']
-#p=PostProcessor(".",inputfile,"",modules=[leptonConstr(),puAutoWeight_2018()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
+# inputfile = ['/afs/cern.ch/user/p/pellicci/cernbox/ROOT/ZEMuAnalysis/NANOAOD/191120_10218V1/ZEMuAnalysis_pythia8_NANOAOD_2018_1.root']
+if doLocal:
+    inputfile = ['/eos/uscms/store/user/mmackenz/LFVAnalysis_ZMuTau_2016_8028V1/LFVAnalysis_NANOAOD_8028V1/200716_213639/0000/ZEMuAnalysis_pythia8_NANOAOD_2016_1.root']
+    # inputfile = ['/eos/uscms/store/user/mmackenz/LFVAnalysis_HEMu_2016_8028V1/LFVAnalysis_NANOAOD_8028V1/200708_163332/0000/ZEMuAnalysis_pythia8_NANOAOD_2016_1.root']
+    # inputfile = ['/eos/uscms/store/user/mmackenz/LFVAnalysis_HMuTau_2016_8028V1/LFVAnalysis_NANOAOD_8028V1/200716_180028/0000/ZEMuAnalysis_pythia8_NANOAOD_2016_1.root']
+    p=PostProcessor(".",inputfile,"",modules=[leptonConstr(0)],provenance=True,fwkJobReport=True,outputbranchsel="cmssw_config/keep_and_drop.txt")
 
-if isData :
-    if year == "2016" :
-        p=PostProcessor(".",inputFiles(),modules=[leptonConstr(0)],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
-    elif year == "2017" :
-        p=PostProcessor(".",inputFiles(),modules=[leptonConstr(1)],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
-    elif year == "2018" :
-        p=PostProcessor(".",inputFiles(),modules=[leptonConstr(2)],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
-else :
-    if year == "2016" :
-        p=PostProcessor(".",inputFiles(),modules=[leptonConstr(0),puAutoWeight_2016()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
-    elif year == "2017" :
-        p=PostProcessor(".",inputFiles(),modules=[leptonConstr(1),puAutoWeight_2017()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
-    elif year == "2018" :
-        p=PostProcessor(".",inputFiles(),modules=[leptonConstr(2),puAutoWeight_2018()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
+else:
+    if isData :
+        if year == "2016" :
+            p=PostProcessor(".",inputFiles(),modules=[leptonConstr(0)],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
+        elif year == "2017" :
+            p=PostProcessor(".",inputFiles(),modules=[leptonConstr(1)],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
+        elif year == "2018" :
+            p=PostProcessor(".",inputFiles(),modules=[leptonConstr(2)],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
+    else :
+        if year == "2016" :
+            p=PostProcessor(".",inputFiles(),modules=[leptonConstr(0),puAutoWeight_2016()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
+        elif year == "2017" :
+            p=PostProcessor(".",inputFiles(),modules=[leptonConstr(1),puAutoWeight_2017()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
+        elif year == "2018" :
+            p=PostProcessor(".",inputFiles(),modules=[leptonConstr(2),puAutoWeight_2018()],provenance=True,fwkJobReport=True,jsonInput=runsAndLumis(),outputbranchsel="cmssw_config/keep_and_drop.txt")
 
 p.run()
 
